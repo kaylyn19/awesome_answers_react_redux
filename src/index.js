bfa4a2fab84d8271e49d621e712cfa8221bc9eb2
Brandon Lam <brandonywlam@gmail.com>
2019-07-30 09:34:39 -0700

    cleanup repository

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function HelloWorld(props) {
+  const { name } = props;
+  return (
+    <div>Hello {name || "World"}!</div>
+  )
+}
+
+// Attatches Our Root React Component to the DOM
+// Render components inside of JSX as if they are tags
+// Add data to props object like adding attributes to tags
+
+ReactDOM.render(<HelloWorld name='Jon Snow' />, document.getElementById('root'));
+// The above line is similar to:
+// HelloWorld({name: "Jon Snow"})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
