ea5bf2bb6126c6095bf7a6a207887bd969d3254b
Brandon Lam <brandonywlam@gmail.com>
2019-07-30 09:36:12 -0700

    add QuestionDetails

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-function HelloWorld(props) {
-  const { name } = props;
+function QuestionDetails() {
   return (
-    <div>Hello {name || "World"}!</div>
-  )
+    <div>
+      <h2>What is your favourite color?</h2>
+      <p>
+        Red, green, blue, magenta, etc. <br />
+        By Bridge Troll
+      </p>
+      <p>
+        <small>Seen 10 time(s)</small> – <small>Created 10 days ago</small>
+      </p>
+    </div>
+  );
 }
 
-// Attatches Our Root React Component to the DOM
-// Render components inside of JSX as if they are tags
-// Add data to props object like adding attributes to tags
-
-ReactDOM.render(<HelloWorld name='Jon Snow' />, document.getElementById('root'));
-// The above line is similar to:
-// HelloWorld({name: "Jon Snow"})
+ReactDOM.render(<QuestionDetails/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
